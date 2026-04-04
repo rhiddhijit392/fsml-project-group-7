c95d7b1d844bd9ee01ef2402d2faf8e3b7cc6949
Aparichit0 <93782780+Aparichit0@users.noreply.github.com>
2026-04-04 19:14:11 +0530

    better labeling of 'Target' variable

--- src/features.py
+++ src/features.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
 def build_features(df):
+    df = df.copy()
+
     # 1. Daily return
     df['Daily_Return'] = (df['Close'] - df['Open']) / df['Open']
 
@@ -15,10 +17,9 @@ def build_features(df):
     # 4. Volume change per company
     df['Volume_Change'] = df.groupby('Company')['Volume'].pct_change()
 
-    # 5. Target variable
-    df['Target'] = (
-        df.groupby('Company_encoded')['Close']
-        .shift(-1) > df['Close']
+    # 5. Tommorow's closing > today's closing
+    df['Next_Day_UP'] = (
+        df.groupby('Company')['Close'].shift(-1) > df['Close']
     ).astype(int)
 
     # 6. Drop NaN rows
