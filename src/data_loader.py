b79bc454a8d7ec59f16bbba99761a800e5793480
Aparichit0 <93782780+Aparichit0@users.noreply.github.com>
2026-04-04 15:56:12 +0530

    updated filepath, assuming dataset is stored in data/

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,7 +1,17 @@
-import numpy as np
+import os
 import pandas as pd
 
+# This starts from the project folder (/Users/zer0/vscode/fsml-project-group-7)
+filepath = os.path.join(os.getcwd(), "data/stock_details_5_years.csv")
+
 def load_data(filepath):
-    df = pd.read_csv(filepath)
-    print(f"Data loaded: {df.shape[0]} rows, {df.shape[1]} columns")
-    return df
+    try:
+        df = pd.read_csv(filepath)
+        print(f"Data loaded: {df.shape[0]} rows, {df.shape[1]} columns")
+        return df
+    except FileNotFoundError:
+        print(f"Error: 'stock_details_5_years.csv' not found in {os.getcwd()}")
+        return None
+
+if __name__ == "__main__":
+    df = load_data(filepath)
