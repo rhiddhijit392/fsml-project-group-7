c3703a7f066a4d274733d7096c17d066452a2a24
Aparichit0 <93782780+Aparichit0@users.noreply.github.com>
2026-04-15 04:20:56 +0530

    added evaluation and predict to pipeline

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -10,11 +10,13 @@ from src.data_loader import load_data
 from src.preprocess import preprocess
 from src.features import build_features
 from src.train import run_training
+from src.evaluate import run_evaluation
+from src.predict import run_prediction
 
 def run_pipeline():
     # Step 1: Load data
     df = load_data()
-    print("Columns after load_data:", df.columns.tolist())
+    print("Columns loaded from data:", df.columns.tolist())
 
     # Step 2: Preprocess
     df = preprocess(df)
@@ -25,13 +27,22 @@ def run_pipeline():
     print("Columns after build_features:", df.columns.tolist())
     
     #Pipeline upto DE work
-    print(f"Data Pipeline complete: {df.shape[0]} rows, {df.shape[1]} columns")
+    print(f"Data Pipeline complete!\n processed data: {df.shape[0]} rows, {df.shape[1]} columns")
+    print(f"Sample Data Stream:\n{df.head()}\n{'='*55}\n")
 
     # Step 4: Train model
-    run_training(df)
+    model, X_train, X_test, y_train, y_test, test_idx = run_training(df)
+    print("Training pipeline complete!\n", "="*55)
+
+    #step 5: Evaluation
+    run_evaluation(df, model=model, X_test=X_test, y_test=y_test, test_indices=test_idx)
+    print("Evaluation complete!\n", "="*55)
+
+    #step 6: Prediction
+    run_prediction(df, model=model)
+    print("prediction complete!\n", "="*55)
     
     return df
 
 if __name__ == "__main__":
     df = run_pipeline()
-    print(df.head())
