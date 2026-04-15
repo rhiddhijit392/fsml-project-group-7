40fff66d601e82b809ae6842da20bb86770d882d
Aparichit0 <93782780+Aparichit0@users.noreply.github.com>
2026-04-15 06:41:08 +0530

    shifting to use only upto yesterday's values

--- src/features.py
+++ src/features.py
@@ -2,20 +2,21 @@ import pandas as pd
 
 def build_features(df):
     df = df.copy()
+    # Shift all price data by 1 to represent "Yesterday's" data (ensures no "Today" data is used to predict "Tomorrow")
 
-    # 1. Daily return
-    df['Daily_Return'] = (df['Close'] - df['Open']) / df['Open']
+    # 1. Daily return %
+    df['Daily_Return'] = df.groupby('Company')['Close'].pct_change().shift(1)
 
     # 2. Price range
-    df['Price_Range'] = df['High'] - df['Low']
+    df['Price_Range'] = (df['High'] - df['Low']).shift(1)
 
-    # 3. 5 day moving average per company
+    # 3. 5 day moving average
     df['MA_5'] = df.groupby('Company')['Close'].transform(
-        lambda x: x.rolling(window=5).mean()
+        lambda x: x.rolling(window=5).mean().shift(1)
     )
 
-    # 4. Volume change per company
-    df['Volume_Change'] = df.groupby('Company')['Volume'].pct_change()
+    # 4. Volume change %
+    df['Volume_Change'] = df.groupby('Company')['Volume'].pct_change().shift(1)
 
     # 5. Target
     df['Next_Day_Close'] = df.groupby('Company')['Close'].shift(-1)
